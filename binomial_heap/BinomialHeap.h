506c7df69f4370a3662a457c4d434bb9327e75f9
carrollwood2014 <carrollwood2014@gmail.com>
2016-10-15 23:39:59 -0700

    build up binomialHeap data structure

--- binomial_heap/BinomialHeap.h
+++ binomial_heap/BinomialHeap.h
@@ -5,4 +5,44 @@
 #ifndef BASIC_DS_BINOMIALHEAP_H
 #define BASIC_DS_BINOMIALHEAP_H
 
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>
+
+
+namespace  detail {
+    template <typename T> struct  BinomialNode;
+}
+
+template  <typename  T> class BinomialHeap{
+public:
+    BinomialHeap();
+    ~BinomialHeap();
+
+    BinomialHeap(const BinomialHeap&);
+    BinomialHeap& operator= (const BinomialHeap&);
+
+    void push(const T&);
+
+    const T& top() const;
+
+    void pop();
+
+    void merge(BinomialHeap& other);
+
+    size_t size() const;
+    bool empty() const;
+
+    void swap(BinomialHeap& other);
+
+private:
+    std::vector<detail::BinomialNode<T>*> mTrees;
+    size_t mSize;
+
+};
+
+
+
+
+
 #endif //BASIC_DS_BINOMIALHEAP_H
